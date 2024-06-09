4db23332d655895ed3b034b2ae4900d94bef238a
pvocke <80173952+pvocke@users.noreply.github.com>
2024-06-09 03:45:27 -0400

    Update

--- Web-App/Backend/app.js
+++ Web-App/Backend/app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
 
 const postsRoutes = require("./routes/posts");
 const usersRoutes = require("./routes/user")
--- Web-App/src/app/posts/post.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Post {
-    id: string,
-    name: string;
-    email: string;
-    date: string;
-    content: string;
-    imagePath: string;
-}
